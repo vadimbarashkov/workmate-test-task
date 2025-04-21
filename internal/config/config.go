97a6aa2ac8b74edb651ea3affe7aecb556b860e5
Vadim Barashkov <vadimdominik2005@gmail.com>
2025-04-21 19:46:39 +0300

    feat: add basic config validation logic

--- internal/config/config.go
+++ internal/config/config.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"os"
 
+	"github.com/go-playground/validator/v10"
 	"github.com/goccy/go-yaml"
 )
 
@@ -14,11 +15,17 @@ const (
 )
 
 type Config struct {
-	Env string `yaml:"env"`
+	Env string `yaml:"env" validate:"required,oneof=dev test prod"`
 }
 
 var defaultConfig = Config{
 	Env: EnvDev,
+}
+
+var validate = validator.New()
+
+func (c *Config) validate() error {
+	return validate.Struct(c)
 }
 
 func Load(path string) (*Config, error) {
@@ -31,6 +38,10 @@ func Load(path string) (*Config, error) {
 	cfg := defaultConfig
 	if err := yaml.NewDecoder(f).Decode(&cfg); err != nil {
 		return nil, fmt.Errorf("decode config file: %w", err)
+	}
+
+	if err := cfg.validate(); err != nil {
+		return nil, fmt.Errorf("invalid config: %w", err)
 	}
 
 	return &cfg, nil
