a3f4ab97b5d861d57d92c5fac884b28e55b7e7f9
Vadim Barashkov <vadimdominik2005@gmail.com>
2025-04-21 18:15:26 +0300

    feat: add executor to in-memory task manager

--- internal/manager/memory/manager.go
+++ internal/manager/memory/manager.go
@@ -2,39 +2,50 @@ package memory
 
 import (
 	"context"
+	"fmt"
 	"sync"
 
 	"github.com/google/uuid"
 	"github.com/vadimbarashkov/workmate-test-task/internal/entity"
+	"github.com/vadimbarashkov/workmate-test-task/internal/executor"
 	"github.com/vadimbarashkov/workmate-test-task/internal/manager"
 )
 
 type TaskManager struct {
-	mu    sync.RWMutex
-	tasks map[string]*entity.Task
+	tasks    map[string]*entity.Task
+	executor executor.Executor
+	mu       sync.RWMutex
 }
 
-func New() *TaskManager {
+func New(executor executor.Executor) *TaskManager {
 	return &TaskManager{
-		tasks: make(map[string]*entity.Task),
+		tasks:    make(map[string]*entity.Task),
+		executor: executor,
 	}
 }
 
-func (m *TaskManager) Create(_ context.Context, taskType entity.TaskType, payload []byte) (uuid.UUID, error) {
+func (m *TaskManager) Create(ctx context.Context, taskType entity.TaskType, payload []byte) (uuid.UUID, error) {
 	task := entity.NewTask(taskType, payload)
+
+	if err := m.executor.Execute(ctx, task); err != nil {
+		return uuid.Nil, fmt.Errorf("execute task: %w", err)
+	}
+
 	m.mu.Lock()
 	m.tasks[task.ID().String()] = task
 	m.mu.Unlock()
-	// TODO: add to queue
+
 	return task.ID(), nil
 }
 
 func (m *TaskManager) Get(_ context.Context, taskID uuid.UUID) (*entity.Task, error) {
 	m.mu.RLock()
+	defer m.mu.RUnlock()
+
 	task, exists := m.tasks[taskID.String()]
 	if !exists {
 		return nil, manager.ErrTaskNotFound
 	}
-	m.mu.RUnlock()
+
 	return task, nil
 }
